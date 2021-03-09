5a19943cb8bdb5293a7c4e675da345926e4006d0
Kevin Zehnder <51463990+righttrianglesrkewl3@users.noreply.github.com>
2021-03-08 16:35:26 -0800

    Create classPractice3.cpp

---
+++ c++/inheritance/classPractice3.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <limits>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+class YouTube {
+private:
+    string ProtocolUsed;
+    int YearCreated;
+    
+    static int TotalUsers;
+
+public:
+    // constructors
+    YouTube();
+    YouTube(string ProtocolUsed, int YearCreated);
+    ~YouTube();
+    void SetAll(string ProtocolUsed, int YearCreated);
+
+    // get/set
+    void SetProtocolUsed(string ProtocolUsed) {this->ProtocolUsed = ProtocolUsed;}
+    string GetProtocolUsed() {return ProtocolUsed;}
+    void SetYearCreated(int YearCreated) {this->YearCreated = YearCreated;}
+    int GetYearCreated() {return YearCreated;}
+
+    // get/set static int
+    static int GetTotalUsers() {return TotalUsers;}
+
+    // created to be overridden
+    void ToString();
+};
+
+int YouTube::TotalUsers = 0;
+
+// no data passed default constructor
+YouTube::YouTube(){
+
+}
+
+// default
+YouTube::YouTube(string ProtocolUsed, int YearCreated){
+    this->ProtocolUsed = ProtocolUsed;
+    this->YearCreated = YearCreated;
+    TotalUsers ++;
+
+}
+
+YouTube::~YouTube(){
+    cout << "This object has been destroyed! " << endl;
+}
+
+// set all
+void YouTube::SetAll(string ProtocolUsed, int YearCreated){
+
+}
+
+// ToString()
+void YouTube::ToString(){
+    cout << "Protocol used = " << this->ProtocolUsed << endl;
+    cout << "Created = " << this->YearCreated << endl;
+    cout << "Total User Num = " << this->TotalUsers << endl;
+}
+
+// static int defined in class
+// Dont do this here!
+/*
+// GetTotalUsers()
+static int YouTube::GetTotalUsers(){
+
+}
+*/
+
+int main()
+{
+    bool TestBool;
+    TestBool = true; // true = 1 for bools
+    cout << TestBool << endl;
+
+    YouTube west ("TCP/IP", 2010);
+    west.ToString();
+}
