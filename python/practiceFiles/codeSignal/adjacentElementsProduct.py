8779d9c7062f22321b435f43cf4b15c544c89a36
Kevin Zehnder <51463990+righttrianglesrkewl3@users.noreply.github.com>
2021-06-03 12:40:07 -0700

    Create adjacentElementsProduct.py

---
+++ python/practiceFiles/codeSignal/adjacentElementsProduct.py
@@ -0,0 +1,57 @@
+"""
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+
+7 and 3 produce the largest product.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+An array of integers containing at least two elements.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 10,
+-1000 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The largest product of adjacent elements.
+
+"""
+
+inputArray = [3, 6, -2, -5, 7, 3]
+
+def adjacentElementsProduct(inputArray):
+    return max(([inputArray[i] * inputArray[i+1] for i in range(len(inputArray) - 1)]))
+
+print(adjacentElementsProduct(inputArray))
+
+
+
+"""
+Non Optimal solution that just shows a cool way to find the max key in a dictionary
+
+def adjacentElementsProducts(inputArray):
+    counts = {}
+    largestProduct = None
+
+    for i in range(len(inputArray) - 1):
+        counts[(inputArray[i], inputArray[i+1])] = inputArray[i] * inputArray[i+1]
+
+    max_key = max(counts, key=counts.get)
+    largestProduct = max_key[0] * max_key[1]
+    
+    return largestProduct
+
+
+inputArray = [3, 6, -2, -5, 7, 3]
+print(adjacentElementsProducts(inputArray))
+
+"""
