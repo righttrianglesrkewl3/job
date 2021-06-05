411cfdf7ca4c1e6b1c28ef20e25c1c05a91ab681
Kevin Zehnder <51463990+righttrianglesrkewl3@users.noreply.github.com>
2021-06-04 19:41:10 -0700

    Delete ideas.py

--- python/todoApp/application/main/ideas.py
+++
@@ -1,7 +0,0 @@
-"""
-Another possible solution specially if you want batch delete
-
-deleted_objects = User.__table__.delete().where(User.id.in_([1, 2, 3]))
-session.execute(deleted_objects)
-session.commit()
-"""
