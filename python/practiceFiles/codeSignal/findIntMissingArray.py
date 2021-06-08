9252ffecc0800642761b985a1c89b01f4147ee01
Kevin Zehnder <51463990+righttrianglesrkewl3@users.noreply.github.com>
2021-06-08 13:44:38 -0700

    Create findIntMissingArray.py

---
+++ python/practiceFiles/codeSignal/findIntMissingArray.py
@@ -0,0 +1,22 @@
+"""
+Question source: https://simpleprogrammer.com/programming-interview-questions/
+Part of common array-based coding interview questions
+
+1. How do you find the missing number in a given integer array of 1 to 100?
+
+"""
+
+# make list with integers from 1 to 100 
+oneToHundredFull = [i for i in range(1, 101)]
+
+# make list with integers from 1 to 100 (except missing one number: 5)
+oneToHundredMissing = [i for i in range(1, 101) if i != 5]
+
+# initialize empty list to append mising number to
+missingNum = []
+
+for i in range(len(oneToHundredFull)):
+    if oneToHundredFull[i] not in oneToHundredMissing:
+        missingNum.append(oneToHundredFull[i])
+
+print(missingNum)
