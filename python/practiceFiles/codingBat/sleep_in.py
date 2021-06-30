500eaeb5c07923b89969e565d5588abdf802764b
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-06-30 12:08:58 -0700

    Create sleep_in.py

---
+++ python/practiceFiles/codingBat/sleep_in.py
@@ -0,0 +1,19 @@
+"""
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True
+"""
+
+def sleep_in(weekday, vacation):
+    bool_sleep = False
+    if weekday != True or vacation == True:
+        bool_sleep = True
+
+    return bool_sleep
+
+print(sleep_in(weekday=False, vacation=False))
+print(sleep_in(weekday=True, vacation=False))
+print(sleep_in(weekday=False, vacation=True))
