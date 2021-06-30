f09391e7582d7609dfb703bf79ec4280918411ec
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-06-30 12:14:46 -0700

    Update sleep_in.py

--- python/practiceFiles/codingBat/sleep_in.py
+++ python/practiceFiles/codingBat/sleep_in.py
@@ -1,3 +1,6 @@
+# 6-30-21
+# kZ
+
 """
 The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
