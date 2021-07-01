b66268c3dd01b87dfd907806d6a58b995e64a4ce
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-01 15:36:02 -0700

    Update __init__.py

--- python/flask-nginx-uwsgi-digital-ocean-webhost/plotlyflask_tutorial/__init__.py
+++ python/flask-nginx-uwsgi-digital-ocean-webhost/plotlyflask_tutorial/__init__.py
@@ -16,14 +16,10 @@ def init_app():
     with app.app_context():
         # Import parts of our core Flask app
         from . import routes
-        from .assets import compile_static_assets
 
         # Import Dash application
         from .plotlydash.dashboard import init_dashboard
 
         app = init_dashboard(app)
-
-        # Compile static assets
-        compile_static_assets(assets)
 
         return app
