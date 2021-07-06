67ab97855614baf8161c3a0c54263108e8c9d91c
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-06 14:28:39 -0700

    Update missing_char.py

--- python/practiceFiles/codingBat/missing_char.py
+++ python/practiceFiles/codingBat/missing_char.py
@@ -16,3 +16,11 @@ print(missing_char('kitten', 1))
 print(missing_char('kitten', 0))
 print(missing_char('kitten', 4))
 
+
+"""
+Solution:
+def missing_char(str, n):
+  front = str[:n]   # up to but not including n
+  back = str[n+1:]  # n+1 through end of string
+  return front + back
+"""
